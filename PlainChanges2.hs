faa72f48626d855a4b029e9448a8aa2d29fe0b76
Mark Lentczner <markl@glyphic.com>
2013-07-27 16:24:24 -0700

    refactor part II, and add some more coil

--- PlainChanges2.hs
+++ PlainChanges2.hs
@@ -120,37 +120,41 @@ preamble = undefined
 fMinorScale :: [PitchClass]
 fMinorScale = [F, G, Af, Bf, C, Df, Ef, F]
 
-arpeggioChord :: Dur -> Dur -> [a] -> Music a
-arpeggioChord d e = chord . zipWith delayedNote [n * e | n <- [0..]]
-  where
-    delayedNote ne p = rest ne :+: note (d - ne) p
 
+b1pitches, b2pitches, b3pitches, b4pitches :: [Pitch]
+b1pitches = [(E,  3), (Df, 3), (Bf, 2), (G, 2)]
+b2pitches = [(C,  4), (Af, 3), (F,  3), (C, 3)]
+b3pitches = [(Df, 4), (C,  4), (Af, 3), (E, 3)]
+b4pitches = [(F,  4), (Ef, 4), (C, 4), (G, 3)]
 
 p2Ostinado :: Music Pitch
 p2Ostinado = onBass
-        $ chord $ zipWith delayedRing starts pitchSets
+    $   riff  0   0    0 b1pitches
+    :=: riff  4   0 (-2) b2pitches
+    :=: riff  8   0 (-1) b3pitches
+    :=: riff 10   4 (-3) b4pitches
   where
-    delayedRing d ps = rest d :+: ringNotes qn ps
-    pitchSets = [ [(E,  3), (Df, 3), (Bf, 2), (G, 2)]
-                , [(C,  4), (Af, 3), (F,  3), (C, 3)]
-                , [(Df, 4), (C,  4), (Af, 3), (E, 3)]
-                , [(F,  4), (Ef, 4), (C, 4), (G, 3)]
-                ]
-    starts = [  0 * 9 * qn - 0 * sn
-             ,  4 * 9 * qn - 2 * sn
-             ,  8 * 9 * qn - 1 * sn
-             , 10 * 9 * qn + 4 * qn - 3 * sn
-             ]
+    riff a b c ps = delayM (a*9*qn + b*qn + c*sn) $ ringNotes qn ps
+
+p2OnCoil :: Music Pitch -> Music Pitch
+p2OnCoil = onCoil . phrase [Art $ Staccato $ 1/4]
 
 p2coil :: Music Pitch
-p2coil = phrase [Art $ Staccato $ 1/4] (onCoil cParts)
+p2coil = p2OnCoil
+    $   riff  2 [(Af, 5), (F,  5), (C, 5)]
+    :=: riff  3 [(C,  6), (Af, 5), (E, 5)]
+    :=: riff  7 [(Ef, 6), (C, 6), (G, 5)]
   where
-    cParts = chord [ cPart (2*9*qn) (sn/2) [(Af, 5), (F,  5), (C, 5)]
-                   , cPart (3*9*qn) (sn/2) [(C,  6), (Af, 5), (E, 5)]
-                   , cPart (7*9*qn) (sn/2) [(Ef, 6), (C, 6), (G, 5)]
-                   ]
-    cPart r d ps = rest r :+: ringNotes d ps
-    cascade = chord . zipWith (flip cPart en) [0,5*en..]
+    riff a ps = delayM (a*9*qn) $ ringNotes (sn/2) ps
+
+p2coilAccentB3 :: Music Pitch
+p2coilAccentB3 = p2OnCoil $ delayM (8*9*qn)
+    $ chord (map acc [4, 6, 10, 11, 12])
+  where
+    acc n = delayM (fromIntegral n*9*qn) $ chord $
+        zipWith accLine [0..] $ take 8 $ drop (n * 8) b3line
+    accLine m (p,o) = delayM (m*qn) $ ringNotes (sn) [(p,o+2)]
+    b3line = concat $ ringOf b3pitches ++ [b3pitches]
 
 p2tempo :: Music a -> Music a
 p2tempo m = tempo (startTempo / 120) mAll
@@ -174,7 +178,7 @@ interruptionB = tempo (170 / 120) $ v :=: b :=: c :=: d
     d = onDrums $ ringPerc t [AcousticSnare, LowTom]
 
 partII :: Music Pitch
-partII = (p2tempo $ p2Ostinado :=: p2coil) -- :+: interruptionB
+partII = (p2tempo $ p2Ostinado :=: p2coil :=: p2coilAccentB3) -- :+: interruptionB
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 -- Part III
